42fd41893095c00c25b4fe85462606bb189ae2cb
Jo Mav <djemsmavoungou@gmail.com>
2025-03-13 08:52:27 +0100

    get_file_2.py updated

--- get_file_2.py
+++ get_file_2.py
@@ -1,7 +1,9 @@
 def user():
     name = input('tape your name: ')
     id_number = int(input("tape your id: "))
+    email = input("tape your email address: username@gmail.com")
     print(f"welcome {name}{id_number}")
+    print(f"{email} is your email address: ")
 
 
 user()
