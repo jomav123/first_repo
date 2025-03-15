fbe02f919d6d6cf4f170f57442fa1eac8a4c90d0
Jo Mav <djemsmavoungou@gmail.com>
2025-03-15 09:44:06 +0100

    updated git_file_2.py

--- git_file_2.py
+++ git_file_2.py
@@ -2,8 +2,10 @@ def user():
     name = input('tape your name: ')
     id_number = int(input("tape your id: "))
     email = input("tape your email address: username@gmail.com")
+    address = input("where do you live?")
     print(f"welcome {name}{id_number}")
     print(f"{email} is your email address: ")
+    print(name,' lives at ', address)
 
 
 user()
